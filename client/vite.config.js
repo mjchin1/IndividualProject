a890b5dea07610a6cf4b7a0671c92e8c15504f1f
Melissa J. Chin <melissa1jchin@gmail.com>
2024-01-28 21:58:30 -0600

    updated target in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:8080/api",
-        target: "https://field-trip-server.onrender.com/api",
+        target: "http://localhost:8080/api",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
