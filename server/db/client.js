fca614df9a4ea6a9267eb4b9086c5a522867fbd7
Melissa J. Chin <melissa1jchin@gmail.com>
2024-04-30 14:46:22 -0500

    updating connection string

--- server/db/client.js
+++ server/db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 
-// const client = new Client(`postgres://localhost:54321/beautifulplaces`);
-const client = new Client(`postgres://mchin:MTJyO01UtuFjF3hHS3eZ7BYsOynlMeP0@dpg-cmrgt0ug1b2c73dbbo3g-a/beautifulplaces`);
+// const client = new Client("postgres://localhost:54321/beautifulplaces");
+const client = new Client("postgres://beautifulplaces_d3jz_user:bg35IdlDXb5dQ6pmjatx0rb6H03NgMZI@dpg-cookid63e1ms73b7k6c0-a/beautifulplaces_d3jz");
 
 module.exports = client;
